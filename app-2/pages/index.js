74facdf77dd04d42596988734181d9e2c031be29
Pravallika <100998718+Pravz-149@users.noreply.github.com>
2023-06-16 13:20:14 +0000

    Now gives summary of data

--- app-2/pages/index.js
+++ app-2/pages/index.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import useLLM from 'usellm';
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [imageData, setImageData] = useState('');
   const [description, setDescription] = useState('');
+  const [summaryAndQuestions, setSummaryAndQuestions] = useState('');
+  const llm = useLLM({
+    serviceUrl: 'https://usellm.org/api/llm',
+  });
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-  
+
     reader.onloadend = () => {
       const base64Data = reader.result;
       setImageData(base64Data);
     };
-  
+
     if (file) {
       reader.readAsDataURL(file);
     }
@@ -28,22 +33,19 @@ export default function Home() {
     try {
       const apiUrl = 'https://api.ocr.space/parse/image';
       const apiKey = 'K88980484188957';
-      //const imageUrl = 'http://dl.a9t9.com/ocrbenchmark/eng.png';
       const { data } = await axios.post(apiUrl, {
         language: 'eng',
         isOverlayRequired: 'false',
         filetype: 'png',
         base64Image: imageData,
         iscreatesearchablepdf: 'false',
-        issearchablepdfhidetextlayer: 'false'
+        issearchablepdfhidetextlayer: 'false',
       }, {
         headers: {
           apikey: apiKey,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-
-      console.log(data)
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
 
       const responseJson = data;
       if (
@@ -63,6 +65,32 @@ export default function Home() {
     }
   };
 
+  async function handleClickGenerateSummaryAndQuestions() {
+    try {
+      setLoading(true);
+
+      const { message } = await llm.chat({
+        messages: [
+          {
+            role: 'user',
+            content: `
+              Generate a brief summary and answer questions if any based on the description:
+
+              ${description}
+
+            `,
+          },
+        ],
+      });
+
+      console.log('Received message: ', message.content);
+      setSummaryAndQuestions(message.content);
+      setLoading(false);
+    } catch (error) {
+      console.error('Something went wrong!', error);
+    }
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <input type="file" accept="image/*" onChange={handleImageUpload} style={{ marginBottom: '16px' }} />
@@ -70,7 +98,20 @@ export default function Home() {
       <button disabled={loading || !imageData} onClick={generateText} style={{ marginTop: '16px' }}>
         {loading ? 'Generating Text...' : 'Generate Text'}
       </button>
-      {description && <p>{description}</p>}
+      {description && (
+        <>
+          <p>{description}</p>
+          <button disabled={loading || !description} onClick={handleClickGenerateSummaryAndQuestions}>
+            {loading ? 'Generating Summary and Questions...' : 'Generate Summary and Questions'}
+          </button>
+        </>
+      )}
+      {summaryAndQuestions && (
+        <div>
+          <p style={{ fontWeight: 'bold' }}>Summary and Questions:</p>
+          <p>{summaryAndQuestions}</p>
+        </div>
+      )}
     </div>
   );
 }
